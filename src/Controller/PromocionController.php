865caee2c46d9df6cd351928e588adb1fcb04d17
Marta García Ortega <mgarort-555@g.educaand.es>
2022-05-21 14:05:19 +0200

    Creation Contacto form and change of template and Controller

--- src/Controller/PromocionController.php
+++ src/Controller/PromocionController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactoType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PromocionController extends AbstractController
 {
     #[Route('/promocion', name: 'app_promocion')]
     public function index(): Response
     {
+        $form = $this->createForm(ContactoType::class);
+
         return $this->render('promocion/index.html.twig', [
-            'controller_name' => 'PromocionController',
+            'form' => $form->createView(),
         ]);
     }
 }
