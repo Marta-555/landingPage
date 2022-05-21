e4706f16d6baa46feabf14e4df433defdcc8e1cd
Marta García Ortega <mgarort-555@g.educaand.es>
2022-05-21 14:17:13 +0200

    Saving data to DB

--- src/Controller/PromocionController.php
+++ src/Controller/PromocionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacto;
 use App\Form\ContactoType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,9 +13,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PromocionController extends AbstractController
 {
     #[Route('/promocion', name: 'app_promocion')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
-        $form = $this->createForm(ContactoType::class);
+        $contact = new Contacto();
+        $form = $this->createForm(ContactoType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+
+        }
 
         return $this->render('promocion/index.html.twig', [
             'form' => $form->createView(),
