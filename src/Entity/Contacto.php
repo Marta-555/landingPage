e2353b9f27d250b3180eaf678edccf8369ed7f0f
Marta García Ortega <mgarort-555@g.educaand.es>
2022-05-22 02:39:02 +0200

    Validation in Contacto entity

--- src/Entity/Contacto.php
+++ src/Entity/Contacto.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContactoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactoRepository::class)]
 class Contacto
@@ -14,24 +15,34 @@ class Contacto
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
     private $nombre;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
     private $apellidos;
 
     #[ORM\Column(type: 'integer', nullable: true)]
+    #[Assert\LessThan(10)]
     private $telefono;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private $email;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Choice(['Turismo', 'Todoterreno', 'Comercial'])]
     private $tipoVehiculo;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Choice(['Astra', 'Corsa', 'Mokka'])]
     private $vehiculo;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Assert\Choice(['Mañana', 'Tarde', 'Noche'])]
     private $preferenciaLlamada;
 
     public function getId(): ?int
